399ff79203e2fbf0e09d2fc14fcaf391dbfa055f
Rich Paret <623643+rparet@users.noreply.github.com>
2024-06-20 10:36:55 -0700

    misc bug fixes

--- src/aliases/recall.lua
+++ src/aliases/recall.lua
@@ -1,8 +1,9 @@
 -- emergency recall, assumes you're already remorted.
-if msdp.CLASS == "Curist" and msdp.LEVEL >= 12 then
-    send("sling 'world of recall'")
-else
-    local bag = Nf.profile.bag.one or "bag"
-    send("get recall " .. bag)
-    send("recite recall")
-end
+send("recall")
+-- if msdp.CLASS == "Curist" and msdp.LEVEL >= 12 then
+--     send("sling 'world of recall'")
+-- else
+--     local bag = Nf.profile.bag.one or "bag"
+--     send("get recall " .. bag)
+--     send("recite recall")
+-- end
---
+++ src/triggers/Curist/chatHeal.lua
@@ -0,0 +1,1 @@
+send("sling 'restoration' Larz")
